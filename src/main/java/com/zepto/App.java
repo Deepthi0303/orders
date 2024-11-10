581b076da9b5a452a059e7805ddd03ea63cd895a
Deepthi0303 <deepusreedhar2002@gmail.com>
2024-11-10 22:48:01 +0530

    Some changes

--- src/main/java/com/zepto/App.java
+++ src/main/java/com/zepto/App.java
@@ -7,7 +7,5 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hey there");
-        System.out.println("Heyyyyyyyy");
-        System.out.println("Just like that");
     }
 }
